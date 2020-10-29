ac54f388e2e40f8a1fe887ff366b816f621e10a5
blsoko <exanymejose1@hotmail.com>
2020-10-29 15:42:48 -0500

    kwargs and args

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,23 @@ class BaseModel():
     """
     Defines a BaseModel
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a BaseModel
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = self.created_at
+        if kwargs:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                            "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                            "%Y-%m-%dT%H:%M:%S.%f")
+            for key, value in kwargs.items():
+                if key is "__class__":
+                    continue
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
